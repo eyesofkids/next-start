dc571504fd2d9634fd8480a214ae095b9fc0a0bc
eyesofkids <eddy@joomla.com.tw>
2024-11-16 23:01:21 +0800

    T

--- app/test/page.js
+++ app/test/page.js
@@ -1,13 +1,11 @@
-'use client'
-
 import './test.css'
-
-import React, { useState, useEffect } from 'react'
+import MyButton from '@/components/ui/my-button'
 
 export default function TestPage(props) {
   return (
     <>
       <div>Test Page</div>
+      <MyButton />
     </>
   )
 }
---
+++ components/ui/my-button.js
@@ -0,0 +1,9 @@
+import './test.css'
+
+export default function MyButton(props) {
+  return (
+    <>
+      <button className="test">MyButton</button>
+    </>
+  )
+}
