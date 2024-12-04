c529b659e15384bce612799fa6e703fdff556a91
Eddy Chang <eddy@joomla.com.tw>
2024-12-04 16:44:21 +0800

    U

--- app/shop/[slug]/page.js
+++ app/shop/[slug]/page.js
@@ -1,8 +1,8 @@
-
-export default async function SlugPage({params}) {
+export default async function SlugPage({ params }) {
   // 例如: shop/1	可以得到 Promise<{ slug: '1' }>
   console.log(params)
-  const slug = (await params).slug
+  // const slug = (await params).slug
+  const slug = params?.slug
+
   return <h1>slug: {slug}</h1>
 }
-
